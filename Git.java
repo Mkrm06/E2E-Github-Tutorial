780bb4ae1bf3802ccb7bf7cf7207570c27ae63b8
Mohamed Mukarram Ismath <159976191+Mkrm06@users.noreply.github.com>
2025-02-10 07:51:29 +0530

    Update Git.java

--- Git.java
+++ Git.java
@@ -1,5 +1,6 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello World! This is Devop");
+        System.out.println("This is Devop");
     }    
 }
